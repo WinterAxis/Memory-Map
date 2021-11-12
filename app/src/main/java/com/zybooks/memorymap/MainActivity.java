8bcbb9e9b6c2ed19a706486ed27dc404151748ac
WinterAxis <78036640+WinterAxis@users.noreply.github.com>
2021-11-11 21:02:46 -0500

    Added Navigation
    Added navigation using fragments similar to what we did for the lights out fragment.

--- app/src/main/java/com/zybooks/memorymap/MainActivity.java
+++ app/src/main/java/com/zybooks/memorymap/MainActivity.java
@@ -1,14 +1,34 @@
 package com.zybooks.memorymap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
+                    R.id.navigation_home, R.id.navigation_map_list, R.id.navigation_map_editor)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+            NavigationUI.setupWithNavController(navView, navController);
+        }
     }
 }
