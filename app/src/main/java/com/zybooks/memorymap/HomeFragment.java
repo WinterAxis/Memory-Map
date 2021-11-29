760ff2eeafe6af2f4bb8ebda047c1697d5714000
WinterAxis <78036640+WinterAxis@users.noreply.github.com>
2021-11-29 16:37:45 -0500

    Fixed Failed Commit

--- app/src/main/java/com/zybooks/memorymap/HomeFragment.java
+++ app/src/main/java/com/zybooks/memorymap/HomeFragment.java
@@ -1,102 +1,54 @@
 package com.zybooks.memorymap;
 
-<<<<<<< Updated upstream
-import android.os.Bundle;
-
-=======
 import android.os.Build;
 import android.os.Bundle;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
->>>>>>> Stashed changes
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< Updated upstream
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-=======
 import android.widget.Button;
 
 
 @RequiresApi(api = Build.VERSION_CODES.P)
 public class HomeFragment extends Fragment {
 
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View parentView = inflater.inflate(R.layout.fragment_home, container, false);
 
         Button newMapBtn = parentView.findViewById(R.id.new_map_button);
+        Log.d("Event", "newMapBtn: found?"+newMapBtn);
         newMapBtn.setOnClickListener(v -> newMap(v));
 
         Button listMapBtn = parentView.findViewById(R.id.list_map_button);
         listMapBtn.setOnClickListener(v -> {
+            Log.d("Event", "listMapBtn: clicked?");
             Navigation.findNavController(v).navigate(R.id.navigation_map_list);
         });
->>>>>>> Stashed changes
 
-    public HomeFragment() {
-        // Required empty public constructor
+        return parentView;
     }
 
-<<<<<<< Updated upstream
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-=======
     private void newMap(View view) {
+        Log.d("Event", "newMap: Here");
         Navigation.findNavController(view).navigate(R.id.navigation_map_editor);
     }
 
-
-
->>>>>>> Stashed changes
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
 }
--- app/src/main/java/com/zybooks/memorymap/MapEditorFragment.java
+++ app/src/main/java/com/zybooks/memorymap/MapEditorFragment.java
@@ -1,41 +1,19 @@
 package com.zybooks.memorymap;
 
-<<<<<<< Updated upstream
 import android.os.Bundle;
 
-=======
 import android.graphics.Bitmap;
 import android.graphics.ImageDecoder;
 import android.os.Build;
-import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
->>>>>>> Stashed changes
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< Updated upstream
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapEditorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MapEditorFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-=======
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -45,60 +23,29 @@ import java.io.IOException;
 @RequiresApi(api = Build.VERSION_CODES.P)
 public class MapEditorFragment extends Fragment {
 
->>>>>>> Stashed changes
 
     public MapEditorFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapEditorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapEditorFragment newInstance(String param1, String param2) {
-        MapEditorFragment fragment = new MapEditorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< Updated upstream
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-=======
->>>>>>> Stashed changes
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-<<<<<<< Updated upstream
-        return inflater.inflate(R.layout.fragment_map_editor, container, false);
-    }
-=======
+
         View parentView = inflater.inflate(R.layout.fragment_map_editor, container, false);
 
         Button setMapBtn = parentView.findViewById(R.id.set_map);
-        setMapBtn.setOnClickListener(v -> setMap(v));
+        setMapBtn.setOnClickListener(v -> mGetImageContent.launch("image/*"));
         return parentView;
     }
 
-    private void setMap(View view) {
-        mGetImageContent.launch("image/*");
-    }
 
     private final ActivityResultLauncher<String> mGetImageContent = registerForActivityResult(
             new ActivityResultContracts.GetContent(),
@@ -106,13 +53,11 @@ public class MapEditorFragment extends Fragment {
               try {
                     ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), uri);
                     Bitmap bitmap = ImageDecoder.decodeBitmap(source);
-                    ImageView imageView = getActivity().findViewById(R.id.map_image1);
+                    ImageView imageView = getActivity().findViewById(R.id.map_image);
                     imageView.setImageBitmap(bitmap);
                 } catch (IOException e) {
 
                 }
             }
     );
-
->>>>>>> Stashed changes
 }
