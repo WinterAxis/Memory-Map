cfd80b502cadf70d5be0c040c8b8311742249df8
WinterAxis <78036640+WinterAxis@users.noreply.github.com>
2021-11-30 22:11:24 -0500

    Added Pins Loading From Pref File

--- app/src/main/java/com/zybooks/memorymap/MapEditorFragment.java
+++ app/src/main/java/com/zybooks/memorymap/MapEditorFragment.java
@@ -2,6 +2,7 @@ package com.zybooks.memorymap;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.graphics.Bitmap;
@@ -11,9 +12,14 @@ import android.os.Build;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -21,11 +27,14 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @RequiresApi(api = Build.VERSION_CODES.P)
@@ -44,7 +53,6 @@ public class MapEditorFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        Log.d("TAG", "onAttach: "+Map_ID);
     }
 
     @Override
@@ -53,9 +61,7 @@ public class MapEditorFragment extends Fragment {
 
         Bundle args = getArguments();
         if (args != null) {
-            Log.d("TAG", "onCreate:Before "+Map_ID);
             Map_ID = args.getString(ARG_MAP_ID);
-            Log.d("TAG", "onCreate:After "+Map_ID);
         }
         map_pref = getContext().getSharedPreferences(Map_ID, 0);
 
@@ -67,45 +73,100 @@ public class MapEditorFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View parentView = inflater.inflate(R.layout.fragment_map_editor, container, false);
+        ConstraintLayout parentLayout  = (ConstraintLayout) parentView.findViewById(R.id.ConstraintLayout_map_editor);
+        ConstraintSet conSet = new ConstraintSet();
+
 
         Button setMapBtn = parentView.findViewById(R.id.set_map);
         setMapBtn.setOnClickListener(v -> mGetImageContent.launch("image/*"));
 
-        //Load Pins from Preferences
+        //add some pins to test delete later
         SharedPreferences.Editor editor = map_pref.edit();
-        //Name To Be Changed Latter
-        editor.putString("Test", "true");
+        Set<String> set = new HashSet<String>();
+        set.add("pin1");
+        set.add("pin2");
+//        set.add("pin3");
+        editor.putStringSet("Pins", set);
+        editor.putInt("pin1_marginStart", 200);
+        editor.putInt("pin1_marginTop", 300);
+        editor.putString("pin1_Title", "Test1");
+        editor.putString("pin1_Description", "Should load this");
+
+        editor.putInt("pin2_marginStart", 400);
+        editor.putInt("pin2_marginTop", 500);
+        editor.putString("pin2_Title", "Test2");
+        editor.putString("pin2_Description", "Description");
+        
+        editor.putInt("Next_Pin_Id", 4);
         editor.apply();
-        Log.d("TAG", "onCreateView: "+editor);
+
+        ImageView mapImg = getActivity().findViewById(R.id.map_image);
+
+        //Retrieve the values
+        Set<String> pins = map_pref.getStringSet("Pins", null);
+
+        //Load Pins
+        for (String pin : pins){
+
+            Log.d("TAG", "onCreateView: "+pin);
+            ImageView newPin = new ImageView(getContext());
+            newPin.setId(View.generateViewId());
+            newPin.setImageResource(R.drawable.pin_place);
+            newPin.setColorFilter(ContextCompat.getColor(getContext(), R.color.blue));
+            newPin.setTag(pin);
+            newPin.setElevation(10);
+
+            newPin.setOnClickListener(v -> { onButtonShowPopupWindowClick(v); });
+
+            parentLayout.addView(newPin, 0);
+
+            conSet.clone(parentLayout);
+
+            // connect start and end point of views, in this case top of child to top of parent.
+            conSet.connect(newPin.getId(), ConstraintSet.TOP, parentLayout.getId(), ConstraintSet.TOP, map_pref.getInt(pin+"_marginTop", 0));
+            conSet.connect(newPin.getId(), ConstraintSet.START, parentLayout.getId(), ConstraintSet.START, map_pref.getInt(pin+"_marginStart", 0));
+            conSet.applyTo(parentLayout);
+        }
 
         //to be removed later just a pin for testing
-        ImageView tempPin = parentView.findViewById(R.id.temp);
-        tempPin.setOnClickListener(v -> { onButtonShowPopupWindowClick(v); });
+//        ImageView tempPin = parentView.findViewById(R.id.temp);
+//        tempPin.setOnClickListener(v -> { onButtonShowPopupWindowClick(v); });
 
         return parentView;
     }
 
-
-    private final ActivityResultLauncher<String> mGetImageContent = registerForActivityResult(
-            new ActivityResultContracts.GetContent(),
-            uri -> {
-              try {
-                    ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), uri);
-                    Bitmap bitmap = ImageDecoder.decodeBitmap(source);
-                    ImageView imageView = getActivity().findViewById(R.id.map_image);
-                    imageView.setImageBitmap(bitmap);
-                } catch (IOException e) {
-
-                }
-            }
-    );
-
     public void onButtonShowPopupWindowClick(View view) {
+        Log.d("TAG", "onButtonShowPopupWindowClick: "+view.getTag());
+        String pin = (String) view.getTag();
+
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(getContext().LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.pin_popup_window, null);
 
-        //
+        // set the windows text
+        EditText title = popupView.findViewById(R.id.pin_title);
+        title.setText(map_pref.getString(pin+"_Title", "Title"));
+        title.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                SharedPreferences.Editor editor = map_pref.edit();
+                editor.putString(pin+"_Title", String.valueOf(s));
+                editor.apply();
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+        });
+
+        EditText dis = popupView.findViewById(R.id.pin_description);
+        dis.setText(map_pref.getString(pin+"_Description", "Description"));
+        dis.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                SharedPreferences.Editor editor = map_pref.edit();
+                editor.putString(pin+"_Description", String.valueOf(s));
+                editor.apply();
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+        });
 
         // create the popup window
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
@@ -118,7 +179,6 @@ public class MapEditorFragment extends Fragment {
         }
 
         // show the popup window
-        // which view you pass in doesn't matter, it is only used for the window token
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
         // dismiss the popup window when touched
 //        popupView.setOnTouchListener(new View.OnTouchListener() {
@@ -129,4 +189,19 @@ public class MapEditorFragment extends Fragment {
 //            }
 //        });
     }
+
+    //Gets Image from the gallery and sets it to the map image
+    private final ActivityResultLauncher<String> mGetImageContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            uri -> {
+                try {
+                    ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), uri);
+                    Bitmap bitmap = ImageDecoder.decodeBitmap(source);
+                    ImageView imageView = getActivity().findViewById(R.id.map_image);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+
+                }
+            }
+    );
 }
